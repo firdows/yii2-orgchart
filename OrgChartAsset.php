64fc9caa9baf12dc12d1c059ffff90815d459e1b
Jehdu Ahamad <ahamad.jedu@gmail.com>
2017-01-26 18:01:57 +0700

    Create OrgChartAsset.php

---
+++ OrgChartAsset.php
@@ -0,0 +1,22 @@
+<?php 
+namespace firdows\orgchart; 
+use yii\web\AssetBundle; 
+
+
+class OrgChartAsset extends AssetBundle { 
+    public $sourcePath = '@bower/orgchart/dist'; //กำหนดที่เก็บ Asset(css,js,image) 
+    public $css = [  //กำหนดลงทะเบียนไฟล์ css 
+      'jquery.orgchart.css',
+    ]; 
+    //public $cssOptions = ['position' => \yii\web\View::POS_HEAD];
+  
+    public $js = [ //กำหนดลงทะเบียนไฟล์ javascript 
+      'jquery.orgchart.js', 
+    ]; 
+
+    public $depends = [ //กำหนดให้ลงทะเบียนหลังจาก Asset เหล่านี้ 
+      'yii\bootstrap\BootstrapPluginAsset',
+    ]; 
+  
+    //public $publishOptions = ['forceCopy' => true];
+}
