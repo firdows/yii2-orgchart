262cc02da78994c455301bdc9253d91dfef2adfb
Firdows <ahamad.jedu@gmail.com>
2017-05-02 22:50:34 +0000

    add Html2Convas

--- Html2CanvasAsset.php
+++ Html2CanvasAsset.php
@@ -3,20 +3,20 @@ namespace firdows\orgchart;
 use yii\web\AssetBundle; 
 
 
-class OrgChartAsset extends AssetBundle { 
-    public $sourcePath = '@bower/orgchart/dist'; //กำหนดที่เก็บ Asset(css,js,image  ) 
+class Html2CanvasAsset extends AssetBundle { 
+    public $sourcePath = '@bower/html2canvas/build'; //กำหนดที่เก็บ Asset(css,js,image  ) 
     public $css = [  //กำหนดลงทะเบียนไฟล์ css 
-      'css/jquery.orgchart.css',
+     // 'css/jquery.orgchart.css',
     ]; 
     //public $cssOptions = ['position' => \yii\web\View::POS_HEAD];
   
     public $js = [ //กำหนดลงทะเบียนไฟล์ javascript 
-      'js/jquery.orgchart.js', 
+      'html2canvas.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.debug.js',
     ]; 
 
     public $depends = [ //กำหนดให้ลงทะเบียนหลังจาก Asset เหล่านี้ 
       'yii\bootstrap\BootstrapPluginAsset',
-      'firdows\orgchart\Html2CanvasAsset',
     ]; 
   
     //public $publishOptions = ['forceCopy' => true];
